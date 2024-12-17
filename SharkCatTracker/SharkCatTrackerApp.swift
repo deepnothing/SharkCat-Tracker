63e97fb64fbbf137b4b016b50db3b43cf988f7ec
deepnothing <teoudovcic@gmail.com>
2024-12-16 23:07:51 -0500

    Initial Commit

---
+++ SharkCatTracker/SharkCatTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharkCatTrackerApp.swift
+//  SharkCatTracker
+//
+//  Created by Teo on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SharkCatTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SharkCatTrackerTests/SharkCatTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SharkCatTrackerTests.swift
+//  SharkCatTrackerTests
+//
+//  Created by Teo on 12/16/24.
+//
+
+import Testing
+
+struct SharkCatTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
