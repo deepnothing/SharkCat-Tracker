eb9ba985997875b2f97df1ef1ee968c936e83e40
deepnothing <teoudovcic@gmail.com>
2024-12-20 15:01:47 -0500

    updates

---
+++ SharkCatWidget/SharkCatWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  SharkCatWidgetBundle.swift
+//  SharkCatWidget
+//
+//  Created by Teo on 12/16/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SharkCatWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SharkCatWidget()
+        SharkCatWidgetControl()
+        SharkCatWidgetLiveActivity()
+    }
+}
